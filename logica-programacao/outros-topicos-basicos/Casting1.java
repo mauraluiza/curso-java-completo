07043302df80baf387e64418829c75a7c55b07b5
Maura Luiza Silva <113071443+mauraluiza@users.noreply.github.com>
2025-01-06 16:38:56 -0300

    Update Casting1.java

--- logica-programacao/outros-topicos-basicos/Casting1.java
+++ logica-programacao/outros-topicos-basicos/Casting1.java
@@ -9,8 +9,7 @@ public class Casting1 {
 		a = 5;
 		b = 2;
 		
-		r = (double) a / b;     // Casting permite a conversão dos dados int para double
-								// para que não haja perda na divisão por inteiros
+		r = (double) a / b;     // Casting permite a conversão dos dados int para double para que não haja perda na divisão por inteiros
 		
 		System.out.print(r);
 		
