af168d8b95849846fb1da397258f14dbe9828e07
Maura Luiza Silva <113071443+mauraluiza@users.noreply.github.com>
2025-05-03 20:04:19 -0300

    Update and rename ProgramaSemPOO.java to ProgramaSemPOO.java

--- poo/introducao/application/ProgramaSemPOO.java
+++ poo/introducao/application/ProgramaSemPOO.java
@@ -1,5 +1,4 @@
 package application;
-
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -7,15 +6,14 @@ public class ProgramaSemPOO {
 	public static void main(String[] args) {
 		
 		/*
-		 * Programa para calcular a área de um triângulo, sem utilizar conceitos de Programação
-		 * Orientada a Objetos. É possível perceber a repetição de códigos sem necessidade, além
-		 * da desorganização e confusão que essa prática pode causar.
+		 * Programa para calcular a área de dois triângulos e comparar a maior área,
+   		 sem utilizar conceitos de Programação Orientada a Objetos.
 		 */
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		double xA, xB, xC, yA, yB, yC; // repetição de variáveis com mesma função
+		double xA, xB, xC, yA, yB, yC; // Repetição de variáveis com mesma função
 		
 		System.out.println("Enter the measures of triangle X: ");
 		xA = sc.nextDouble();
@@ -29,8 +27,8 @@ public class ProgramaSemPOO {
 		double p = (xA + xB + xC) / 2.0; 
 		double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
 		
-		p = (yA + yB + yC) / 2.0; // repetição de fórmula
-		double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC)); // repetição de fórmula
+		p = (yA + yB + yC) / 2.0; // Repetição de fórmula
+		double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC)); // Repetição de fórmula
 		
 		System.out.printf("Triangle X area: %.4f%n", areaX);
 		System.out.printf("Triangle Y area: %.4f%n", areaY);
