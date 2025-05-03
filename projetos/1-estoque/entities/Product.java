ab103e71d5d68b39165c6540ce0b65302c217297
Maura Luiza Silva <113071443+mauraluiza@users.noreply.github.com>
2025-05-03 20:31:44 -0300

    Update projetos/1-estoque/entities/Product.java

--- projetos/1-estoque/entities/Product.java
+++ projetos/1-estoque/entities/Product.java
@@ -1,25 +1,27 @@
 package entities;
 
 public class Product {
-	
+
+	// Atributos da classe:
 	public String name;
 	public double price;
 	public int quantity;
-	
-	public double totalValueInStock() {
+
+	// Métodos da classe:
+	public double totalValueInStock() { // Calcular valor total em estoque
 		return price * quantity;
 	}
 	
-	public void addProducts(int quantity) {
+	public void addProducts(int quantity) { // Adiciona produtos ao estoque utilizando atributo quantity como parâmetro
 		this.quantity += quantity;
 	}
 	 
-	public void removeProducts(int quantity) {
+	public void removeProducts(int quantity) { // Remove produtos do estoque utilizando atributo quantity como parâmetro
 		this.quantity -= quantity;
 	}
 	
 	public String toString() { // toString é um método da classe String capaz de formatar a saída de dados quando identifica um comando de saída na main
-		return name // retorna a seguinte formatação na variável name quando dentro de um print
+		return name // Retorna a seguinte formatação na variável name quando dentro de um print
 				+", $ " 
 				+ String.format("%.2f", price) 
 				+ ", " 
