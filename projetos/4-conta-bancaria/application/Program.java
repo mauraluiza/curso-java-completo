14a27dc0128fe1061ad4ccd75d0c0cfca84c5124
Maura Luiza Silva <113071443+mauraluiza@users.noreply.github.com>
2025-05-03 21:00:02 -0300

    Update and rename projetos/6-conta-bancaria/src/application/Program.java to projetos/4-conta-bancaria/application/Program.java

--- projetos/4-conta-bancaria/application/Program.java
+++ projetos/4-conta-bancaria/application/Program.java
@@ -1,7 +1,6 @@
 package application;
 
 import entities.Account;
-
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -11,7 +10,8 @@ public class Program {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
+		// Informa os atributos em variáveis locais primeiro antes de instanciar o objeto
 		System.out.print("Enter account number: ");
 		int number = sc.nextInt();
 		System.out.print("Enter account holder: ");
@@ -20,8 +20,7 @@ public class Program {
 		System.out.print("Is there an initial deposit (y/n)? ");
 		char resp = sc.next().charAt(0);
 		
-		double value;
-		
+		double value; // Declara o value antes do if para pertencer ao escopo da main e poder ser utilizado mais vezes
 		if (resp == 'y') {
 			System.out.print("Enter a initial deposit value: ");
 			value = sc.nextDouble();
@@ -30,24 +29,23 @@ public class Program {
 		}
 		
 		System.out.println("");
-		
-		Account ac = new Account(number, holder, value);
+		Account ac = new Account(number, holder, value); // Instancia o objeto chamando as variáveis locais como parâmetros
 	
-		System.out.println("Account data:");
+		System.out.println("Account data:"); // Imprime informações da conta
 		System.out.println(ac);
 		System.out.println("");
 		
 		System.out.print("Enter a deposit value: ");
 		double qtd = sc.nextDouble();
-		ac.deposit(qtd);
-		System.out.println("Updated account data:");
+		ac.deposit(qtd); // Recebe qtd como parâmetro para o método deposit
+		System.out.println("Updated account data:"); // Imprime informações atualizadas da conta
 		System.out.println(ac);
 		System.out.println("");
 		
 		System.out.print("Enter a withdraw value: ");
 		qtd = sc.nextDouble();
-		ac.withdraw(qtd);
-		System.out.println("Updated account data:");
+		ac.withdraw(qtd); // Reutiliza qtd agora como parâmetro para o método withdraw
+		System.out.println("Updated account data:"); // Imprime informações atualizadas da conta
 		System.out.println(ac);
 
 		sc.close();
